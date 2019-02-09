37078989263254a2f8863c6389b8090d0e9cd3e2
cc <1962631022@qq.com>
2019-02-09 10:18:20 +0800

    wrote a java file

---
+++ CopyTestByBuf.java
@@ -0,0 +1,31 @@
+package com.io.buffer;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
+public class CopyTestByBuf {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub 
+		Reader reader = new FileReader("D:"+File.separator+"1.txt");
+		BufferedReader br = new BufferedReader(reader);
+		
+		Writer writer = new FileWriter("D:"+File.separator+"2.txt");
+		BufferedWriter bw = new BufferedWriter(writer);
+		
+		int ch = 0;
+		while((ch = br.read())!=-1) {
+			bw.write(ch);
+		}
+		
+		br.close();
+		bw.close();
+	} 
+
+}
